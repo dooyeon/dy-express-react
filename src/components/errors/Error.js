051b4c9980d1bb2a5015303cafa6584d18637f8a
dooyeon <endus0713@naver.com>
2019-10-30 23:49:29 +0900

    error page

---
+++ src/components/errors/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Error extends React.Component {
+    
+    render() {
+        return <header className="header">
+                    <div>
+                        Error
+                    </div>
+                </header>;
+    }
+}
+
+export default Error;
